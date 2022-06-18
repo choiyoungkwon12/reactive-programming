458675cecbaef29895167f09b20d55ec750de8da
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-18 20:27:38 +0900

    토비의 봄tv - 리액티브 프로그래밍-1 : iterable 과 duality(쌍대성) 관계를 가진 observable,observer 구현

--- src/main/java/org/toby/observer/Ob.java
+++ src/main/java/org/toby/observer/Ob.java
@@ -2,8 +2,31 @@ package org.toby.observer;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
 
+@SuppressWarnings("deprecation")
 public class Ob {
+
+    /**
+     * Observable
+     * java 1.0 부터 있던 인터페이스로 9부터는 deprecate 되었지만
+     * reactive 프로그래밍을 학습하기 위해 Duality(iterable <-> Observable) 알아보기 위해 사용
+     */
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++) {
+                // 새로운 변화가 생긴것을 setChanged로 호출
+                setChanged();
+
+                // notify 하면서 해당 데이터를 던질 수 있음
+                notifyObservers(i);                     // push
+                // int i = it.next();와 대응이 될 수 있다.  // pull
+            }
+        }
+    }
 
     public static void main(String[] args) {
         // 이터러블 아래의 경우 1부터 10까지 1씩 더하면 됨.
@@ -26,16 +49,30 @@ public class Ob {
             }
         };
 
-
         // 여기서 오른쪽 변수를 iter를 넣으나 iter2를 넣으나 똑같은 역할을 한다.
 //        for (Integer integer : iter2) {
 //            System.out.println(integer);
 //        }
 
         // java5 이전의 방식
-        for(Iterator<Integer> iterator = iter2.iterator();iterator.hasNext();){
+        /*for(Iterator<Integer> iterator = iter2.iterator();iterator.hasNext();){
             System.out.println(iterator.next());
-        }
+        }*/
+
+        // Source -> Event/Data -> Observer(관찰자)한테 던짐
+        // Source인 옵저버블을 만들고, Observer를 만들어서 옵저버블에 등록하고 이벤트가 발생됐을 때 등록되어있는 옵저버들한테 알림을 보낸다.
+        // Reactive Streams에서는 Observer를 subscriber, Observable을 publisher라고도 한다.
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(arg);
+            }
+        };
+
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        io.run();
     }
 
 }
