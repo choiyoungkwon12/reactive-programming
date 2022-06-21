b84c2dc6016fc0d3261f7600a77d153b1e15570a
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-22 01:59:07 +0900

    토비의 봄 reactive-streams-2 Operators
    mapPub을 중간에 사용한 코드

--- src/main/java/org/toby/reactivestreams/PubSub.java
+++ src/main/java/org/toby/reactivestreams/PubSub.java
@@ -1,17 +1,73 @@
 package org.toby.reactivestreams;
 
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
-
+/**
+ * Reactive Streams - Operators
+ * <p>
+ * Publisher -> Data -> Subscriber
+ */
 public class PubSub {
 
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            Iterable<Integer> iter = Stream.iterate(1, i -> i + 1).limit(10).collect(Collectors.toList());
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, i -> i + 1).limit(10).collect(Collectors.toList()));
+
+        /*
+         * pub -> [Data1] -> mapPub -> [Data2] -> logSub
+         *                  <- subscribe(logsub)
+         *                   -> onSubscribe(s)
+         *                   -> onNext
+         *                   -> onNext
+         *                  -> onComplete
+         */
+
+        Publisher<Integer> mapPub = mapPub(pub, (Function<Integer, Integer>)s -> s * 10);
+        mapPub.subscribe(logSub());
+    }
+
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub,
+        Function<Integer, Integer> integerIntegerFunction) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> s) {
+                pub.subscribe(s);
+            }
+        };
+    }
+
+    private static Subscriber<Integer> logSub() {
+        return new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                System.out.println("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext : " + integer);
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+    }
+
+    private static Publisher<Integer> iterPub(Iterable<Integer> iter) {
+        return new Publisher<Integer>() {
 
             @Override
             public void subscribe(Subscriber<? super Integer> sub) {
@@ -21,7 +77,7 @@ public class PubSub {
                         try {
                             iter.forEach(sub::onNext);
                             sub.onComplete();
-                        }catch (Throwable t){
+                        } catch (Throwable t) {
                             sub.onError(t);
                         }
                     }
@@ -33,28 +89,5 @@ public class PubSub {
                 });
             }
         };
-
-        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription s) {
-                s.request(Long.MAX_VALUE);
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-            }
-
-            @Override
-            public void onError(Throwable t) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-
-        pub.subscribe(subscriber);
     }
 }
