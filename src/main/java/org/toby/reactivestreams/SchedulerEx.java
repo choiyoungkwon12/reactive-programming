ef349436bb182f6088a9c859d1803d071faaf4c3
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-26 18:42:06 +0900

    토비의 봄tv - 리액티브 프로그래밍-3 : 퍼블리셔, subscriber 작업을 하나의 스레드가 아닌 다른 스레드에서 처리하도록 구현
    subscribeOn, publishOn 둘다 구현 후 사용

--- src/main/java/org/toby/reactivestreams/SchedulerEx.java
+++ src/main/java/org/toby/reactivestreams/SchedulerEx.java
@@ -10,34 +10,62 @@ public class SchedulerEx {
 
     public static void main(String[] args) {
         Publisher<Integer> publisher = s -> {
-          s.onSubscribe(new Subscription() {
-              @Override
-              public void request(long n) {
-                  s.onNext(1);
-                  s.onNext(2);
-                  s.onNext(3);
-                  s.onNext(4);
-                  s.onNext(5);
-                  s.onComplete();
-              }
+            s.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    System.out.println(Thread.currentThread().getName() + " : request");
+                    s.onNext(1);
+                    s.onNext(2);
+                    s.onNext(3);
+                    s.onNext(4);
+                    s.onNext(5);
+                    s.onComplete();
+                }
 
-              @Override
-              public void cancel() {
+                @Override
+                public void cancel() {
 
-              }
-          });
+                }
+            });
         };
 
         Publisher<Integer> subOnPub = sub -> {
             ExecutorService ex = Executors.newSingleThreadExecutor();
             ex.execute(() -> {publisher.subscribe(sub);});
             ex.shutdown();
-
         };
-        subOnPub.subscribe(new Subscriber<Integer>() {
+
+        Publisher<Integer> pubOnPub = sub -> {
+            subOnPub.subscribe(new Subscriber<Integer>() {
+                ExecutorService es = Executors.newSingleThreadExecutor();
+                @Override
+                public void onSubscribe(Subscription s) {
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    es.execute(() ->sub.onNext(integer));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    es.execute(() -> sub.onError(t));
+                    es.shutdown();
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(sub::onComplete);
+                    es.shutdown();
+                }
+            });
+        };
+
+        pubOnPub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
-                System.out.println("onSubscribe");
+                System.out.println(Thread.currentThread().getName() + " : onSubscribe");
                 s.request(Long.MAX_VALUE);
             }
 
