d2bc2413127cb3a20c1e908ae783cb08708141e7
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-26 19:01:02 +0900

    토비의 봄tv - 리액티브 프로그래밍-3 : 리액터의 flux scheduler 사용해서 subscribeOn 사용

--- src/main/java/org/toby/reactivestreams/FluxEx.java
+++ src/main/java/org/toby/reactivestreams/FluxEx.java
@@ -1,12 +1,14 @@
 package org.toby.reactivestreams;
 
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 public class FluxEx {
 
     public static void main(String[] args) {
         Flux.range(1,10)
             .log()
+            .subscribeOn(Schedulers.newSingle("sub"))
             .subscribe(System.out::println);
     }
 }
