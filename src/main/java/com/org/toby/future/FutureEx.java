5e78c52043735b9f6ab6553eb18490339d1bb546
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-28 00:35:00 +0900

    비동기 작업의 결과를 가져오기 위한 기본적인 인터페이스 - Future
    FutureTask를 이용해서 Future를 callback 방식으로

--- src/main/java/com/org/toby/future/FutureEx.java
+++ src/main/java/com/org/toby/future/FutureEx.java
@@ -1,5 +1,7 @@
 package com.org.toby.future;
 
+import java.util.Objects;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,11 +11,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FutureEx {
 
+    interface SuccessCallback{
+        void onSuccess(String result);
+    }
+    public static class CallbackFutureTask extends FutureTask<String>{
+        SuccessCallback sc;
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback successCallback) {
+            super(callable);
+            this.sc = Objects.requireNonNull(successCallback);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
+        CallbackFutureTask f = new CallbackFutureTask(() -> {
+            Thread.sleep(2000);
+            log.debug("Async");
+            return "Hello";
+        }, System.out::println);
+
         // Future를 callback 방식으로
-        FutureTask<String> f = new FutureTask<>(() -> {
+        /*FutureTask<String> f = new FutureTask<>(() -> {
             Thread.sleep(2000);
             log.debug("Async");
             return "Hello";
@@ -28,7 +55,7 @@ public class FutureEx {
                     e.printStackTrace();
                 }
             }
-        };
+        };*/
 
         es.execute(f);
         es.shutdown();
