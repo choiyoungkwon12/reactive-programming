b32736a726ef93b443ef061a6c9a93c2a9c1cc4c
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-28 00:22:14 +0900

    비동기 작업의 결과를 가져오기 위한 기본적인 인터페이스 - Future
    Future를 callback 방식으로

--- src/main/java/com/org/toby/future/FutureEx.java
+++ src/main/java/com/org/toby/future/FutureEx.java
@@ -5,28 +5,25 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FutureEx {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Future<Integer> future = new CompletableFuture<>();
-        // cached 스레드 풀은 maximum 제한이 없고, 처음에는 스레드가 미리 만들어있지 않고 요청시 생성
         ExecutorService es = Executors.newCachedThreadPool();
 
-        // f는 사실 비동기 작업의 리턴 값은 아니고 리턴값을 얻어올 방법을 제공할 핸들러 같은 것이지 사실 리턴값은 아님.
-        Future<String> f = es.submit(() -> {
+        // Future를 callback 방식으로
+        FutureTask<String> f = new FutureTask<>(() -> {
             Thread.sleep(2000);
             log.debug("Async");
-            es.shutdown();
             return "Hello";
         });
 
-        // future의 get은 submit의 비동기 작업이 완료될때까지 기다림. 그래서 async -> hello -> exit 순서로 log가 찍힘
-        // future의 get은 Blocking 메서드임.
-        // future의 isDone은 작업의 결과를 기다리지 않고 작업이 끝났으면 true, 안끝났으면 false를 나타내는 함수임.
-        // 루프를 돌면서 future의 작업이 끝났는지 isDone으로 확인하고 끝났으면 f.get해서 가져오고 안끝났으면 다른 작업을 하는 방법도 있음.
+        // 비동기 작업을 실행
+        es.execute(f);
+
         System.out.println(f.isDone());
         Thread.sleep(2500);
         log.debug("Exit");
