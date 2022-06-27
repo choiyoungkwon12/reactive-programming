8582959ab658952dd12adacfd0bf65530e2e7d11
choiyoungkwon12 <hpwh4457@naver.com>
2022-06-28 01:11:46 +0900

    스프링에서 10년전에 사용하던 비동기 작업을 위한 코드
    `@Async`를 활용, 비동기 작업을 수행하기 위해서 사용되던 자바가 제공하던 클래스들이나 코드들이 다 사라짐.
    거의 비즈니스 목적에 맞는 오브젝트를 다루기 위한 호출코드임.
    단지 hello() 결과를 Future로 받음

--- src/main/java/com/org/TobyApplication.java
+++ src/main/java/com/org/TobyApplication.java
@@ -1,13 +1,54 @@
 package com.org;
 
+import java.util.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@EnableAsync
 @SpringBootApplication
 public class TobyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TobyApplication.class, args);
+    private final MyService myService;
+
+    public TobyApplication(MyService myService) {
+        this.myService = myService;
     }
+
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext c = SpringApplication.run(TobyApplication.class, args);) {
+        }
+    }
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> {
+            log.info("run()");
+            Future<String> result = myService.hello();
+            log.info("exit : {}", result.isDone());
+            log.info("result : " + result.get());
+        };
+    }
+
+
+    @Component
+    public static class MyService {
+
+        @Async
+        public Future<String> hello() throws InterruptedException {
+            log.info("hello()");
+            Thread.sleep(2000);
+            return new AsyncResult<>("hello");
+        }
+    }
+
 
 }
