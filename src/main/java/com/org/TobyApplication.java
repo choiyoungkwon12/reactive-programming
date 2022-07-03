77127938732cc4f9cef4b31c2b629b9e1f047a28
choiyoungkwon12 <hpwh4457@naver.com>
2022-07-04 00:42:22 +0900

    토비의 봄 - reactive-programming(5)
    비동기적인 내부 작업 추가

--- src/main/java/com/org/TobyApplication.java
+++ src/main/java/com/org/TobyApplication.java
@@ -4,11 +4,17 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,41 +29,6 @@ public class TobyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TobyApplication.class, args);
-    }
-
-    @Slf4j
-    @RestController
-    public static class MyController {
-
-        AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
-
-        @GetMapping("/rest")
-        public DeferredResult<String> rest(int idx) {
-            // 오브젝트를 만들어서 컨트롤러에서 리턴하면 언제가 될지 모르지만 언제인가 DeferredResult에 값을 써주면
-            // 그 값을 응답으로 사용
-            DeferredResult<String> dr = new DeferredResult<>();
-
-            ListenableFuture<ResponseEntity<String>> f1 = rt.getForEntity("http://localhost:8081/service?req={req}", String.class,
-                "hello" + idx);
-            log.info("rest3");
-            f1.addCallback(s -> {
-                log.info("rest1");
-                ListenableFuture<ResponseEntity<String>> f2 = rt.getForEntity("http://localhost:8081/service2?req={req}",
-                    String.class, s.getBody());
-                log.info("rest2");
-                f2.addCallback(s2 -> {
-                    log.info("응답 옴");
-                    dr.setResult(s2.getBody());
-                }, e -> {
-                    dr.setErrorResult(e.getMessage());
-                });
-
-            }, e -> {
-                dr.setErrorResult(e.getMessage());
-            });
-
-            return dr;
-        }
     }
 
     @GetMapping("/emitter")
@@ -84,6 +55,66 @@ public class TobyApplication {
             Thread.sleep(2000);
             return "hello";
         };
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor myThreadPool() {
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(1);
+        te.setMaxPoolSize(10);
+        te.initialize();
+        return te;
+    }
+
+    @Slf4j
+    @RestController
+    public static class MyController {
+
+        @Autowired
+        MyService myService;
+
+        AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
+
+        @GetMapping("/rest")
+        public DeferredResult<String> rest(int idx) {
+            // 오브젝트를 만들어서 컨트롤러에서 리턴하면 언제가 될지 모르지만 언제인가 DeferredResult에 값을 써주면
+            // 그 값을 응답으로 사용
+            DeferredResult<String> dr = new DeferredResult<>();
+
+            ListenableFuture<ResponseEntity<String>> f1 = rt.getForEntity("http://localhost:8081/service?req={req}", String.class,
+                "hello" + idx);
+            log.info("rest3");
+            f1.addCallback(s -> {
+                log.info("rest1");
+                ListenableFuture<ResponseEntity<String>> f2 = rt.getForEntity("http://localhost:8081/service2?req={req}",
+                    String.class, s.getBody());
+                log.info("rest2");
+                f2.addCallback(s2 -> {
+                    ListenableFuture<String> f3 = myService.work(s2.getBody());
+                    f3.addCallback(s3 -> {
+                        dr.setResult(s3);
+                    }, ex -> {
+                        dr.setErrorResult(ex.getMessage());
+                    });
+                }, e -> {
+                    dr.setErrorResult(e.getMessage());
+                });
+
+            }, e -> {
+                dr.setErrorResult(e.getMessage());
+            });
+
+            return dr;
+        }
+    }
+
+    @Service
+    public static class MyService {
+
+        @Async
+        public ListenableFuture<String> work(String req) {
+            return new AsyncResult<>(req + "/asyncwork");
+        }
     }
 }
 
