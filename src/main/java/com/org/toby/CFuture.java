62d7450e3d7da9c836c84fb10bbf487d8c729279
choiyoungkwon12 <hpwh4457@naver.com>
2022-07-06 02:08:32 +0900

    토비의 봄 - reactive-programming-7
    비동기로 서로 다른 스레드에서 처리하는 법

--- src/main/java/com/org/toby/CFuture.java
+++ src/main/java/com/org/toby/CFuture.java
@@ -2,6 +2,8 @@ package com.org.toby;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
@@ -10,27 +12,30 @@ import lombok.extern.slf4j.Slf4j;
 public class CFuture {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(10);
+
         CompletableFuture
             .supplyAsync(() -> {
                 log.info("runAsync");
-                if (1==1) throw new RuntimeException();
                 return 1;
             })
             .thenCompose(s -> {
                 log.info("thenApply {}", s);
                 return CompletableFuture.completedFuture(s + 1);
             })
-            .thenApply(s2 -> {
+            .thenApplyAsync(s2 -> {
                 log.info("thenApply {}", s2);
                 return s2 * 3;
-            })
+            }, es)
             .exceptionally(e -> -10)
-            .thenAccept(s3 -> log.info("thenAccept {} ", s3));
+            .thenAcceptAsync(s3 -> log.info("thenAccept {} ", s3), es);
 
         log.info("exit");
 
         ForkJoinPool.commonPool().shutdown();
         ForkJoinPool.commonPool().awaitTermination(10, TimeUnit.SECONDS);
+
+        es.shutdown();
     }
 
 }
